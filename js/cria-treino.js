3abff82c85d7f8f252d4a74866c4fab251a5186e
AndreRicardoSc <andrenarutto7@gmail.com>
2024-09-09 09:15:09 -0300

    banner implementado

--- js/cria-treino.js
+++ js/cria-treino.js
@@ -1,4 +1,5 @@
 function pegarInfo() {
     const restricao = document.roteiroPerguntas.elements["restricao"].value;
-    
+    const objetivo = document.roteiroPerguntas.elements["objetivo"].value;
+    alert(`Você é ${restricao} e vai ${objetivo}`);
 }
