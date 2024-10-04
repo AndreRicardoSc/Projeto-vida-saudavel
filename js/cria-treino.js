fc334d07145c0ce96c464b3041e84a6e588f3fa4
AndreRicardoSc <andrenarutto7@gmail.com>
2024-10-04 14:54:28 -0300

    produção em massa

--- js/cria-treino.js
+++ js/cria-treino.js
@@ -1,9 +1,9 @@
-    let preferencia = document.getElementById("preferencia").value;
-    let temRestricao = document.getElementById("temRestricao").value;
-    let divAparecer = document.getElementById("ocult");
-    function aparecerRestricao() {
-        divAparecer.style.display = "block"
-    }
-    function criarTreino() {
-        
-    }
+let preferencia = document.getElementById("preferencia").value;
+let temRestricao = document.getElementById("temRestricao").value;
+let divAparecer = document.getElementById("ocult");
+function aparecerRestricao() {
+    divAparecer.style.display = "block"
+}
+function criarTreino() {
+
+}
